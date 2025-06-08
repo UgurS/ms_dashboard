32b853fd18750b5fcf12afedd50b7a7cc62004db
UgurS <42215100+UgurS@users.noreply.github.com>
2025-06-08 18:43:40 +0200

    Add frontend deployment workflow and Update and password_hash column type

--- backend/models/admin.py
+++ backend/models/admin.py
@@ -7,7 +7,7 @@ class Admin(db.Model):
     __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
 
     def to_dict(self):
         return {"id": self.id, "username": self.username}
